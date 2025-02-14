3231fb0b6742bc7e36299225d8afc1f1a41a32a6
okino shogo <okisho0106@gmail.com>
2025-02-15 08:35:28 +0900

    Initial Commit

---
+++ Marubatu/MarubatuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarubatuApp.swift
+//  Marubatu
+//
+//  Created by 沖野匠吾 on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct MarubatuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
