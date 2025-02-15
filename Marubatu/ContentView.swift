4e475492ec24d066c96e088de159359bd1dd6184
okino shogo <okisho0106@gmail.com>
2025-02-15 12:29:31 +0900

    追加機能を作成

--- Marubatu/ContentView.swift
+++ Marubatu/ContentView.swift
@@ -23,69 +23,102 @@ struct ContentView: View {
         Quiz(question: "Xcode画面の右側にはユーティリティーズがある", answer: true),
         Quiz(question: "Textは文字列を表示する際に利用する", answer: true)
     ]
+    
+    @AppStorage("quiz") var quizzesData = Data()//UserDefaultsから問題を読み込む(Data型)
+    @State var quizzesArray: [Quiz] = []//問題を入れておく配列
+    
     @State var currentQuestionNum = 0//今、何問目の数字
     @State var showingAlert = false //アラートの表示・非表示を制御
     @State var alertTitle = ""//"正解"か"不正解"の文字を入れるための変数
     
+    //画面生成時にquizzesDateに読み込んだDate型の値を[Quiz]型にデコードしてquizzesArrayに入れる
+    init(){
+        if let decodedQuizzes = try? JSONDecoder().decode([Quiz].self, from: quizzesData){
+            _quizzesArray = State(initialValue: decodedQuizzes)
+        }
+    }
+    
     var body: some View {
         GeometryReader { geometry in
-            VStack {
-                Text(showQuestion()) // 問題文を表示
-                    .padding()//余白を外側に追加
-                    .frame(width: geometry.size.width*0.85, alignment: .leading)//横幅を250、左寄せに
-                    .fontDesign(.rounded) //フォントのデザインを変更
-                    .background(.yellow)
-                
-                Spacer()
-                //0Xボタンを横並びにするためにHStackを使う
-                HStack{
-                    Button {
-                        //                        print("◯")//ボタンが押された時の動作
-                        checkAnswer(yourAnswer: true)
-                    } label: {
-                        Text("◯")//ボタンの見た目
-                    }
-                    .frame(width: geometry.size.width*0.4, height: geometry.size.width*0.4)//幅と高さを190に
-                    .font(.system(size:100, weight: .bold))//フォントサイズを100、太字に
-                    .background(.red)//背景を赤に
-                    .foregroundStyle(.white)//文字の色を白に
+            NavigationStack{
+                VStack {
+                    Text(showQuestion()) // 問題文を表示
+                        .padding()//余白を外側に追加
+                        .frame(width: geometry.size.width*0.85, alignment: .leading)//横幅を250、左寄せに
+                        .fontDesign(.rounded) //フォントのデザインを変更
+                        .background(.yellow)
                     
-                    Button {
-                        //                        print("×")//ボタンが押された時の動作
-                        checkAnswer(yourAnswer: false)
-                    } label: {
-                        Text("×")//ボタンの見た目
+                    Spacer()
+                    //0Xボタンを横並びにするためにHStackを使う
+                    HStack{
+                        Button {
+                            //                        print("◯")//ボタンが押された時の動作
+                            checkAnswer(yourAnswer: true)
+                        } label: {
+                            Text("◯")//ボタンの見た目
+                        }
+                        .frame(width: geometry.size.width*0.4, height: geometry.size.width*0.4)//幅と高さを190に
+                        .font(.system(size:100, weight: .bold))//フォントサイズを100、太字に
+                        .background(.red)//背景を赤に
+                        .foregroundStyle(.white)//文字の色を白に
+                        
+                        Button {
+                            //print("×")//ボタンが押された時の動作
+                            checkAnswer(yourAnswer: false)
+                        } label: {
+                            Text("×")//ボタンの見た目
+                        }
+                        .frame(width: geometry.size.width*0.4, height: geometry.size.width*0.4)//幅と高さを親ビューの幅の0.4倍に
+                        .font(.system(size:100, weight: .bold))//フォントサイズを100、太字に
+                        .background(.blue)//背景を赤に
+                        .foregroundStyle(.white)//文字の色を白に
+                        
                     }
-                    .frame(width: geometry.size.width*0.4, height: geometry.size.width*0.4)//幅と高さを親ビューの幅の0.4倍に
-                    .font(.system(size:100, weight: .bold))//フォントサイズを100、太字に
-                    .background(.blue)//背景を赤に
-                    .foregroundStyle(.white)//文字の色を白に
+                }
+                .padding()
+                .navigationTitle("マルバツクイズ") // ナビゲーションバーにタイトル設定
+                //回答時のアラート
+                .alert(alertTitle, isPresented: $showingAlert){
+                    Button("OK",role: .cancel){}
+                }
+                .toolbar {
                     
+                    ToolbarItem(placement: .topBarTrailing) {
+                        NavigationLink{
+                            CreateView(quizzesArray: $quizzesArray)
+                                .navigationTitle("問題を作ろう")
+                        }label:{
+                            Image(systemName: "plus")
+                                .font(.title)
+                        }
+                    }
                 }
             }
-            .padding()
-            //ズレを直すために親ビューのサイズをVSrackに適用
-            .frame(width: geometry.size.width,height:geometry.size.height)
-            //回答時のアラート
-            .alert(alertTitle, isPresented: $showingAlert){
-                Button("OK",role: .cancel){}
-            }
+            
         }
     }
     
     //問題文を表示させるための関数
     func showQuestion() -> String {
-        let question = quizeExamples[currentQuestionNum].question
+        var question = "問題がありません"
+        
+        if !quizzesArray.isEmpty {
+            let quiz = quizzesArray[currentQuestionNum]
+            question = quiz.question
+        }
         return question
     }
     
     //回答をチェックする関数、正解なら次の問題を表示
     func checkAnswer(yourAnswer: Bool) {
-        let quiz = quizeExamples[currentQuestionNum]
+        if quizzesArray.isEmpty {
+            return
+        }
+        let quiz = quizzesArray[currentQuestionNum]
         let ans = quiz.answer
         if yourAnswer == ans {
             alertTitle = "正解！"
-            if currentQuestionNum + 1 < quizeExamples.count {
+            if currentQuestionNum + 1 < quizzesArray.count {
                 currentQuestionNum += 1 //次の問題に進む
             }else {
                 //超えたときは0に戻す
---
+++ Marubatu/CreateView.swift
@@ -0,0 +1,97 @@
+//
+//  CreateView.swift
+//  Marubatu
+//
+//  Created by 沖野匠吾 on 2025/02/15.
+//
+
+import SwiftUI
+
+
+struct CreateView: View {
+    @Binding var quizzesArray: [Quiz] // 回答画面で読み込んだ問題を受け取る
+    @State private var questionText = ""//テキストフィールドの文字を受け取る
+    @State private var selectAnswer = "O"
+    let answers = ["O","X"]//ピッカーの選択肢一覧
+    var body: some View {
+        
+        VStack {
+            Text("問題文と解答を入力して、追加ボタンを押してください。")
+                .foregroundStyle(.gray)
+                .padding()
+            
+            //問題文を入力するテキストフィールド
+            TextField(text: $questionText){
+                Text("問題文を入力してください")
+            }
+            .padding()
+            .textFieldStyle(.roundedBorder)
+            
+            Picker("解答", selection: $selectAnswer) {
+                ForEach(answers, id: \.self){answers in
+                    Text(answers)
+                }
+            }
+            .pickerStyle(.segmented)
+            .padding()
+            
+            
+            Button {
+                //追加ボタンが押された時の処理
+                addQuiz(question: questionText, answer: selectAnswer)
+            } label: {
+                Text("追加")
+            }
+            .padding()
+            Button {
+                quizzesArray.removeAll()
+                UserDefaults.standard.removeObject(forKey: "quiz")
+                
+            } label: {
+                Text("全削除")
+            }
+            .foregroundStyle(.red)
+            .padding()
+
+            
+        }
+    }
+    // 問題追加(保存)の関数
+    func addQuiz(question: String, answer: String) {
+        if question.isEmpty{
+            print("プリント文が入力されていません")
+            return
+        }
+        var savingAnswers = true //保存するためのtrue/falseを入れる変数
+        
+        //OかXかで,true/falseを切り替える
+        switch answer {
+        case "O":
+            savingAnswers = true
+        case "X":
+            savingAnswers = false
+        default:
+            print("適切な答えが入ってません")
+            break
+        }
+        
+        let newQuiz = Quiz(question: question, answer: savingAnswers)
+        
+        var array = quizzesArray // 一時的に変数に入れておく
+        array.append(newQuiz)//作った問題を配列に追加
+        let storeKey = "quiz"//UserDefaultsに保存するためのキー
+        
+        //エンコードができたら保存
+        if let encodedQuizzes = try? JSONEncoder().encode(array){
+            UserDefaults.standard.setValue(encodedQuizzes, forKey: storeKey)
+            questionText = ""//テキストフィードを空白に戻す
+            quizzesArray = array // [既存問題 + 新問題]となった配列に更新
+            
+        }
+        
+    }
+}
+
+//#Preview {
+//    CreateView()
+//}
